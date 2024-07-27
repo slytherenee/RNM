f802404d97b0b1bc0e59b51d4be393261b86b159
CloudyG29 <ngmulaudzi2@gmail.com>
2024-07-27 11:52:19 +0000

    initial commit

---
+++ hiresphere/jobs/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Job, Company
+from django.http import HttpResponse
+
+def job_list(request):
+    jobs = Job.objects.all()
+    return render(request, 'jobs/job_list.html', {'jobs': jobs})
+
+def job_detail(request, job_id):
+    job = get_object_or_404(Job, id=job_id)
+    return render(request, 'jobs/job_detail.html', {'job': job})
+
+def create_job(request):
+    if request.method == 'POST':
+        # Add logic to handle job creation form submission
+        return HttpResponse("Job created")
+    return render(request, 'jobs/create_job.html')
